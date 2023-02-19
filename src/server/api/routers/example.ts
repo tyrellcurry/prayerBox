8423486d875b3dbbc1e20470d2993dfec500a1e4
Tyrell C <tyrellrcurry@gmail.com>
2023-02-18 16:44:28 -0800

    Customizing Sign Up Page Additional Minor Adjustments

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -16,6 +16,6 @@ export const exampleRouter = createTRPCRouter({
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+    return "this will only display once you're logged in!";
   }),
 });
