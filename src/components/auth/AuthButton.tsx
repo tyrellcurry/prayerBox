15b15719f1b9668ce3ad901f122fe16868766528
Tyrell C <tyrellrcurry@gmail.com>
2023-02-20 13:30:48 -0800

    Auth showcase changed to button only

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -5,18 +5,13 @@ import { api } from "../../utils/api";
 const AuthShowcase: React.FC = () => {
     const { data: sessionData } = useSession();
   
-    const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-      undefined, // no input
-      { enabled: sessionData?.user !== undefined },
-    );
-  
     return (
-      <div className="flex flex-col items-center justify-center gap-4">
-        {sessionData && <Image className="pt-3" src={sessionData.user?.image ?? ''} width={100} height={100} alt="Profile Image" /> }
+      <div>
+        {/* {sessionData && <Image className="pt-3" src={sessionData.user?.image ?? ''} width={100} height={100} alt="Profile Image" /> }
         <p className="text-center text-2xl text-white">
           {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
           {secretMessage && <span> - {secretMessage}</span>}
-        </p>
+        </p> */}
         <button
           className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
           onClick={sessionData ? () => void signOut() : () => void signIn()}
