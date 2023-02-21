ea74fb4eb5e407ef806006627ae5a090b3102f4c
Tyrell C <tyrellrcurry@gmail.com>
2023-02-21 07:58:41 -0800

    Switched to new API

--- src/components/api/ApiSearch.tsx
+++ src/components/api/ApiSearch.tsx
@@ -1,34 +1,21 @@
 const ApiSearch = (search: string) => {
   var myHeaders = new Headers();
-  myHeaders.append("api-key", "f016cf0177b4aaa543d94e80df34cf26");
-
+  myHeaders.append("Authorization", process.env.NEXT_PUBLIC_API_KEY);
   var requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
   };
-
-  return fetch(
-    `https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/search?query=${search}&limit=20`,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      let offset = Math.floor(Math.random() * (result.data.total + 1));
-      if (offset > 0 && offset === result.data.total) {
-        offset = offset - 1;
-      }
       return fetch(
-        `https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/search?query=${search}&limit=20&offset=${offset}`,
+        `https://api.esv.org/v3/passage/search/?q=${search}&page-size=12&page=1`,
         requestOptions
       )
         .then((response) => response.json())
         .then((result) => {
+          console.log(result)
           return result;
         })
         .catch((error) => console.log("error", error));
-    })
-    .catch((error) => console.log("error", error));
 };
 
 export default ApiSearch;
--- src/components/api/VerseSearch.tsx
+++ src/components/api/VerseSearch.tsx
@@ -16,28 +16,60 @@ const ApiComponent = ({ search }: ApiComponentProps) => {
 
     getData();
   }, [search]);
+  const verses = data?.results;
 
-  const verses = data?.data?.verses;
-
+  //   console.log(passages.length > 0);
+  console.log(verses);
   if (verses === undefined) {
     return <p className="text-center">Loading...</p>;
   } else if (verses.length === 0) {
-    return <p className="text-center">No verses found, please try modifying your search.</p>;
+    return (
+      <p className="text-center">
+        No verses found, please try modifying your search.
+      </p>
+    );
   } else {
     return (
-      <div className="grid grid-cols-2 gap-4">
-        {verses.map((verse: any) => {
-          console.log(verse);
-          return (
-            <div
-              key={verse.id}
-              className="mb-4 rounded-lg bg-slate-300 p-4 text-slate-800 drop-shadow-lg"
-            >
-              <h2 className="font-semibold">{verse.reference}</h2>
-              <p>{verse.text}</p>
-            </div>
-          );
-        })}
+      <div>
+        <p className="pb-2">Page: {data.page} of {data.total_pages}</p>
+        <div className="grid grid-cols-2 gap-4">
+          {verses.map((verse: any) => {
+            return (
+              <div
+                key={verse.reference}
+                className="mb-4 rounded-lg bg-slate-300 p-4 text-slate-800 drop-shadow-lg"
+              >
+                <h2 className="font-semibold">{verse.reference}</h2>
+                <p>{verse.content}</p>
+              </div>
+            );
+          })}
+        </div>
+        <p className="text-xs text-slate-600 pt-1 pb-4">
+          Scripture quotations are from the ESV® Bible (The Holy Bible, English
+          Standard Version®), copyright © 2001 by Crossway, a publishing
+          ministry of Good News Publishers. Used by permission. All rights
+          reserved. The ESV text may not be quoted in any publication made
+          available to the public by a Creative Commons license. The ESV may not
+          be translated into any other language. Users may not copy or download
+          more than 500 verses of the ESV Bible or more than one half of any
+          book of the ESV Bible. Scripture quotations marked “ESV” are from the
+          ESV® Bible (The Holy Bible, English Standard Version®), copyright ©
+          2001 by Crossway, a publishing ministry of Good News Publishers. Used
+          by permission. All rights reserved. The ESV text may not be quoted in
+          any publication made available to the public by a Creative Commons
+          license. The ESV may not be translated into any other language. Users
+          may not copy or download more than 500 verses of the ESV Bible or more
+          than one half of any book of the ESV Bible. Unless otherwise
+          indicated, all Scripture quotations are from the ESV® Bible (The Holy
+          Bible, English Standard Version®), copyright © 2001 by Crossway, a
+          publishing ministry of Good News Publishers. Used by permission. All
+          rights reserved. The ESV text may not be quoted in any publication
+          made available to the public by a Creative Commons license. The ESV
+          may not be translated into any other language. Users may not copy or
+          download more than 500 verses of the ESV Bible or more than one half
+          of any book of the ESV Bible.
+        </p>
       </div>
     );
   }
