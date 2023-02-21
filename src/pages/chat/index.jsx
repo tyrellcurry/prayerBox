5828af239e314faeb114f76b84e6072be176a4d8
Tyrell C <tyrellrcurry@gmail.com>
2023-02-21 15:55:52 -0800

    Moving styles to tailwind - getting ready to make component

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import { useState } from "react";
-import styles from "./index.module.css";
 
 export default function Home() {
   const [prayerInput, setprayerInput] = useState("");
   const [result, setResult] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
+      setIsLoading(true); // Set loading state to true before making API request
+
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
@@ -19,39 +21,46 @@ export default function Home() {
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
       setprayerInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    } finally {
+      setIsLoading(false); // Set loading state back to false after API request is complete
     }
   }
 
-  console.log(result)
+  console.log(result);
 
   return (
     <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-      </Head>
-
-      <main className={styles.main}>
+      <main className="text-center">
         <h3>Request A Prayer</h3>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="flex flex-col items-center">
           <input
+          className="w-96 p-2"
             type="text"
             name="prayer"
             placeholder="What would you like prayer with?"
             value={prayerInput}
             onChange={(e) => setprayerInput(e.target.value)}
           />
-          <input type="submit" value="Generate Prayer" />
+          {!isLoading ? <input className="bg-blue-500 px-4 py-2 rounded-md cursor-pointer" type="submit" value="Generate Prayer" /> : <input type="submit" value="Generate Prayer" className="bg-slate-300 px-4 py-2 rounded-md cursor-pointer" disabled/> }
+          
         </form>
-        <div className={styles.result}>{result}</div>
+        <div className="result">
+        {isLoading && <div>Loading...</div>}
+
+        {!isLoading && result && <div>{result}</div>}
+        </div>
       </main>
     </div>
   );
