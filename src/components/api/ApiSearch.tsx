630225ef020451648f26c025a053ac8ca05bd4db
Tyrell C <tyrellrcurry@gmail.com>
2023-02-20 21:00:38 -0800

    Searches the API for Verses

---
+++ src/components/api/ApiSearch.tsx
@@ -0,0 +1,19 @@
+const ApiSearch = (search: string) => {
+    var myHeaders = new Headers();
+    myHeaders.append("api-key", "f016cf0177b4aaa543d94e80df34cf26");
+  
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+  
+    return fetch(`https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/search?query=${search}&limit=20`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  export default ApiSearch;
