87ffc588b43e33727ec85db8fd7d05d1411a0ec2
Tyrell C <tyrellrcurry@gmail.com>
2023-02-21 13:41:56 -0800

    A prayer request AI chat has been added ✨

---
+++ src/pages/chat/index.jsx
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [prayerInput, setprayerInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prayer: prayerInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setprayerInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  console.log(result)
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h3>Request A Prayer</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="prayer"
+            placeholder="What would you like prayer with?"
+            value={prayerInput}
+            onChange={(e) => setprayerInput(e.target.value)}
+          />
+          <input type="submit" value="Generate Prayer" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
