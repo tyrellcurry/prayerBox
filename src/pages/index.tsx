3035815d83ad414a7a0729019ffe32b69f2c7dd2
Tyrell C <tyrellrcurry@gmail.com>
2023-02-21 18:16:39 -0800

    Fixed TS issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { type NextPage } from "next";
 import Head from "next/head";
-import Nav from "../components/homepage/Nav";
-import Footer from "../components/homepage/Footer";
+import Nav from "../components/layout/Nav";
+import Footer from "../components/layout/Footer";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import AuthShowcase from "../components/auth/AuthButton";
+import AuthButton from "../components/auth/AuthButton";
 import { useSession } from "next-auth/react";
 import { api } from "../utils/api";
 
-const Home: NextPage = () => {
+
+const Home: any = () => {
   const hello = api.example.hello.useQuery({ text: "from PrayerBox" });
 
   const session = useSession();
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
               <p className="text-2xl text-white">
                 {hello.data ? hello.data.greeting : "Loading tRPC query..."}
               </p>
-              <AuthShowcase />
+              <AuthButton />
             </div>
           </div>
         </main>
