9a269ce2eb8a5bb0425ad36b35791500024aaf2e
Tyrell C <tyrellrcurry@gmail.com>
2023-02-24 03:41:26 +0000

    Feb 23, 2023, 7:41 PM

--- src/components/prayerRequest/PrayerRequest.tsx
+++ src/components/prayerRequest/PrayerRequest.tsx
@@ -6,7 +6,7 @@ export default function PrayerRequest() {
   const [result, setResult] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
-  async function onSubmit(event: { preventDefault: () => void; }) {
+  async function onSubmit(event: { preventDefault: () => void }) {
     event.preventDefault();
     try {
       setIsLoading(true); // Set loading state to true before making API request
@@ -42,36 +42,62 @@ export default function PrayerRequest() {
 
   return (
     <div>
-      <main className="text-center flex flex-col items-center px-4">
-        <h3 className="text-2xl pb-4">Receive An Instant Prayer ✨</h3>
+      <main className="flex flex-col items-center px-4 text-center">
+        <h3 className="pb-4 text-2xl">Receive An Instant Prayer ✨</h3>
         <form onSubmit={onSubmit} className="w-full max-w-[350px]">
-          <label className="pb-1" htmlFor="name">Your Name:</label>
-          <input
-          className="w-full p-2 mb-3 rounded-md dark:bg-slate-200 dark:text-slate-800 dark:placeholder-slate-800 "
-            id="name"
-            type="text"
-            name="name"
-            placeholder="What is your name?"
-            value={nameInput}
-            onChange={(e) => setnameInput(e.target.value)}
-            autoComplete="off"
-          />
-          <label className="self-start pb-1" htmlFor="prayer">Your Request:</label>
-          <textarea
-          className="w-full p-2 mb-4 rounded-md min-h-[80px] max-h-[200px] dark:bg-slate-200 dark:text-slate-800 dark:placeholder-slate-800"
-          id="prayer"
-            name="prayer"
-            placeholder="What would you like prayer with? Be as specific as you'd like."
-            value={prayerInput}
-            onChange={(e) => setprayerInput(e.target.value)}
-          />
-          {!isLoading ? <input className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer" type="submit" value="Receive Prayer" /> : <input type="submit" value="Loading..." className="bg-slate-300 text-white px-4 py-2 rounded-md cursor-pointer" disabled/> }
-          
+          <div className="inputs">
+            <label className="pb-1" htmlFor="name">
+              Your Name:
+            </label>
+            <input
+              className="mb-3 w-full rounded-md p-2 dark:bg-slate-200 dark:text-slate-800 dark:placeholder-slate-800 "
+              id="name"
+              type="text"
+              name="name"
+              placeholder="What is your name?"
+              value={nameInput}
+              onChange={(e) => setnameInput(e.target.value)}
+              autoComplete="off"
+            />
+            <label className="self-start pb-1" htmlFor="prayer">
+              Your Request:
+            </label>
+            <textarea
+              className="mb-4 max-h-[200px] min-h-[80px] w-full rounded-md p-2 dark:bg-slate-200 dark:text-slate-800 dark:placeholder-slate-800"
+              id="prayer"
+              name="prayer"
+              placeholder="What would you like prayer with? Be as specific as you'd like."
+              value={prayerInput}
+              onChange={(e) => setprayerInput(e.target.value)}
+            />
+          </div>
+          {!isLoading ? (
+            <input
+              className="cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-white"
+              type="submit"
+              value="Receive Prayer"
+            />
+          ) : (
+            <input
+              type="submit"
+              value="Loading..."
+              className="cursor-pointer rounded-md bg-slate-300 px-4 py-2 text-white"
+              disabled
+            />
+          )}
         </form>
         <div className="">
-        {isLoading && <div className="result mt-4 p-5 max-w-[600px] font-semibold rounded-md bg-slate-50 dark:bg-slate-300 dark:text-slate-800">Your prayer has been received, please wait a moment...</div>}
+          {isLoading && (
+            <div className="result mt-4 max-w-[600px] rounded-md bg-slate-50 p-5 font-semibold dark:bg-slate-300 dark:text-slate-800">
+              Your prayer has been received, please wait a moment...
+            </div>
+          )}
 
-        {!isLoading && result && <div className="result mt-4 p-5 max-w-[600px] font-semibold rounded-md bg-slate-50 dark:bg-slate-300 dark:text-slate-800">{result}</div>}
+          {!isLoading && result && (
+            <div className="result mt-4 max-w-[600px] rounded-md bg-slate-50 p-5 font-semibold dark:bg-slate-300 dark:text-slate-800">
+              {result}
+            </div>
+          )}
         </div>
       </main>
     </div>
