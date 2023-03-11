72e03f3d0f6bc349a9642f0c4b8fb80124f93c06
Tyrell C <tyrellrcurry@gmail.com>
2023-03-11 13:59:48 -0800

    Added navigation to page, and adjusted content to fit the navigation

--- src/pages/journals/index.tsx
+++ src/pages/journals/index.tsx
@@ -1,16 +1,19 @@
+import AppNav from "../../components/layout/AppNav"
 
 function journals() {
     const today = new Date()
     const testTitles = ['Journal Entry One', 'Journal Entry Two']
   return (
     <>
+        <AppNav />
+        <main className="ml-[250px] pt-6">
         <h1 className="text-6xl pb-4">Journal Entries</h1>
         <section>
             <ul>
                 {
                     testTitles.map(titles => (
                         <li className="pb-4">
-                            <a className="block w-fit dark:bg-slate-200 dark:text-slate-800 p-4 rounded-md" href="#">
+                            <a className="block w-fit text-white bg-slate-600 dark:bg-slate-200 dark:text-slate-800 p-4 rounded-md" href="#">
                                 <p>{today.toLocaleDateString()}</p>
                                 <h2>{titles}</h2>
                                 <p>Small description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minima.</p>
@@ -21,6 +24,7 @@ function journals() {
                 
             </ul>
         </section>
+        </main>
     </>
   )
 }
