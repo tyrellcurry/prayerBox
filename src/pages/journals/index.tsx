c6c57df02ba990863bf6c3ce5725c7f6f8dff918
Tyrell C <tyrellrcurry@gmail.com>
2023-03-11 13:54:47 -0800

    Adding some test journal entries for styling

--- src/pages/journals/index.tsx
+++ src/pages/journals/index.tsx
@@ -1,18 +1,24 @@
 
 function journals() {
     const today = new Date()
+    const testTitles = ['Journal Entry One', 'Journal Entry Two']
   return (
     <>
-        <h1 className="text-6xl">Journals</h1>
+        <h1 className="text-6xl pb-4">Journal Entries</h1>
         <section>
             <ul>
-                <li>
-                    <a href="#">
-                        <p>{today.toLocaleDateString()}</p>
-                        <h2>Journal Entry Title</h2>
-                        <p>Small description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minima.</p>
-                    </a>
-                </li>
+                {
+                    testTitles.map(titles => (
+                        <li className="pb-4">
+                            <a className="block w-fit dark:bg-slate-200 dark:text-slate-800 p-4 rounded-md" href="#">
+                                <p>{today.toLocaleDateString()}</p>
+                                <h2>{titles}</h2>
+                                <p>Small description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minima.</p>
+                            </a>
+                        </li>
+                    ))
+                }
+                
             </ul>
         </section>
     </>
